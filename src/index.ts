ade39670ea45b29bbb128a13434171ec48f86599
Satnam Meena <satnam.meena2004@gmail.com>
2025-07-30 02:33:22 +0530

    setup done

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import http from 'http';
+const PORT = 3000;
+
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'application/json' })
+    res.end(JSON.stringify({
+        data: 'Hello World !',
+    }));
+})
+
+server.listen(PORT, () => {
+    console.log("Server listen on port " + PORT + " 🚀")
+})
+
+
+
+
+
