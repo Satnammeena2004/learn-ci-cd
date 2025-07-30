8ddd78a7dec32e56076ba29a0242b2bedd5e20c3
Satnam Meena <satnam.meena2004@gmail.com>
2025-07-30 18:12:14 +0530

    lets go !

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ const server = http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'application/json' })
     res.end(JSON.stringify({
         data: 'Hello World !!',
+        message:"Done 🚀"
     }));
 })
 
